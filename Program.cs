869b52466a2b824b8a44c8d242d1e7e84d8b78cd
sagar <7208sk@gmail.com>
2021-10-19 05:39:48 +0530

    Welcome to LinkedList problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Data structure Problem");
+        }
+    }
+}
